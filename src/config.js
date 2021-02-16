c685cd4bc6df121b456293db739177cfcca0bc05
nmhtet <36500446+nmhtet@users.noreply.github.com>
2021-02-16 20:52:03 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    api_server: 'http://129.213.168.18/',
+    api_server: 'http://193.123.248.46/',
     path_authenticate: 'users/authenticate',
     path_getLatestPosts: 'posts/feed/',
     path_resetPassword: 'users/resetPassword',
@@ -7,6 +7,6 @@ export const config = {
     path_createMessage: 'posts/create',
     path_validate_otp: 'users/validateOTP',
     path_generate_otp: 'users/generateOTP',
-    obj_store_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/p/ShyUyJr14FZAa4rMfwNRvAFgPYzhLWp4IULM_vTv-yQ/n/gse00014894/b/sc_symposium/o/",
-    obj_store_read_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/n/gse00014894/b/sc_symposium/o/",
+    obj_store_url: "https://objectstorage.ap-seoul-1.oraclecloud.com/p/1gZMoIEuYav2_PjoXYaNXY12s_cb7dMEzfZUSIrSgsBtnh2q2Ff9QgHJvssLmkG_/n/cnhcgaz0s1yp/b/nmhtet/o/",
+    obj_store_read_url: "https://objectstorage.ap-seoul-1.oraclecloud.com/p/1gZMoIEuYav2_PjoXYaNXY12s_cb7dMEzfZUSIrSgsBtnh2q2Ff9QgHJvssLmkG_/n/cnhcgaz0s1yp/b/nmhtet/o/",
 }
